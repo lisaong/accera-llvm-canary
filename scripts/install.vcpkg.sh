e4873fdf7856dcd9646a5979442175f44ab183de
Lisa Ong <onglisa@microsoft.com>
2021-12-13 22:52:34 +0800

    fix vcpkg dependencies

--- scripts/install.vcpkg.sh
+++ scripts/install.vcpkg.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e -x
 
+apt-get install -y --no-install-recommends \
+    curl \
+    zip \
+    unzip \
+    tar
+
 /opt/vcpkg/bootstrap-vcpkg.sh
 /opt/vcpkg/vcpkg install catch2 tomlplusplus --overlay-ports=/opt/ports/llvm
 /opt/vcpkg/vcpkg install accera-llvm --overlay-ports=/opt/ports/llvm
