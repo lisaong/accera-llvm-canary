dc266aeb0bd9d21b482af4179f960fe7bc2db1e9
Lisa Ong <lisaong@gmail.com>
2022-02-25 01:38:56 -0800

    tweak build script

--- build.dev.sh
+++ build.dev.sh
@@ -2,7 +2,10 @@
 # Incremental build script
 # Run this after build.sh has run at least once to download llvm and apply patches
 
-pushd external/vcpkg/buildtrees/accera-llvm/x64-linux-rel
+set -x -e
+
+HOME=`pwd`
+cd external/vcpkg/buildtrees/accera-llvm/x64-linux-rel
 cmake --build .
 cmake --build . --target install
-popd
+cd $HOME
