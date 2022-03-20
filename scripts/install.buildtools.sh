4450c3f9a595c9f03665bf1a4a6dbb8779efefca
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2022-03-20 12:20:25 +0800

    Manual multi-arch docker builds to enable arm64 (#17)
    * experimental: build arm64 images
    
    * remove i386 deps
    
    * enable QEMU
    
    * disable cuda for arm64
    
    * pass ARCH as an argument
    
    * remove braces
    
    * typo
    
    * use official variables
    
    * install cmake
    
    * plumb VCPKG_FORCE_SYSTEM_BINARIES for arm64
    
    * install cmake 3.21 for arm64
    
    * fix script
    
    * updated scripts to bash
    
    * update ninja to 1.10.2 for arm64
    
    * update vcpkg submodule
    
    * try bash
    
    * add ntp, cleanup
    
    * more cleanup
    
    * updated readme
    
    * build platforms in parallel
    
    * synchronize to ntp to avoid clock skew
    
    * bump port version
    
    * docs
    
    * Disable docker action for arm64
    
    keep scripts for local build
    
    Co-authored-by: Lisa Ong <onglisa@microsoft.com>

--- scripts/install.buildtools.sh
+++ scripts/install.buildtools.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e -x
 
 export CC=gcc-10
@@ -9,8 +9,7 @@ export CMAKE_CXX_COMPILER=g++-10
 TZ="America/Los_Angeles"
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-dpkg --add-architecture i386 \
-&& apt-get -qq update \
+apt-get -qq update \
 && apt-get install -y --no-install-recommends \
     software-properties-common \
     sudo \
@@ -18,7 +17,6 @@ dpkg --add-architecture i386 \
     git \
     g++-10 \
     make \
-    libc6-dev-i386 \
     libgmp-dev \
     libmpfr-dev \
     libmpc-dev \
@@ -41,18 +39,48 @@ dpkg --add-architecture i386 \
     python3-pip \
     python3-dev \
     libvulkan-dev \
-    libomp-11-dev
+    libomp-dev \
+    cmake \
+    ntp
 
 ln -sf /usr/bin/gcc-10 /usr/bin/gcc \
     && ln -sf /usr/bin/g++-10 /usr/bin/g++ \
     && ln -sf /usr/include/locale.h /usr/include/xlocale.h \
     && rm -rf /var/lib/apt/lists/*
 
-wget -q --no-check-certificate https://cmake.org/files/v3.16/cmake-3.16.4-Linux-x86_64.tar.gz \
-    && tar -xzf cmake-3.16.4-Linux-x86_64.tar.gz \
-       --exclude=bin/cmake-gui \
-       --exclude=doc/cmake \
-       --exclude=share/cmake-3.16/Help \
-    && cp -fR cmake-3.16.4-Linux-x86_64/* /usr \
-    && rm -rf cmake-3.16.4-Linux-x86_64 \
-    && rm cmake-3.16.4-Linux-x86_64.tar.gz
+# Tools like ninja fail when there is clock skew
+ntp -gq
+
+if [[ "$1" = "linux/arm64" ]]; then
+    wget -q --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.21.6/cmake-3.21.6-linux-aarch64.tar.gz \
+        && tar -xzf cmake-3.21.6-linux-aarch64.tar.gz \
+        --exclude=bin/cmake-gui \
+        --exclude=doc/cmake \
+        --exclude=share/cmake-3.21/Help \
+        && cp -fR cmake-3.21.6-linux-aarch64/* /usr \
+        && rm -rf cmake-3.21.6-linux-aarch64 \
+        && rm cmake-3.21.6-linux-aarch64.tar.gz
+
+    # https://github.com/ninja-build/ninja
+    wget https://github.com/ninja-build/ninja/archive/refs/tags/v1.10.2.tar.gz \
+        && tar -xzf v1.10.2.tar.gz \
+        && cd ninja-1.10.2 \
+        && cmake -Bbuild-cmake \
+        && cmake --build build-cmake \
+        && cp -f build-cmake/ninja /usr/bin/ninja \
+        && cp -f /usr/bin/ninja /usr/local/bin/ \
+        && update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force \
+        && cd .. \
+        && rm -rf ninja-1.10.2 \
+        && rm -rf ninja-1.10.2.tar.gz
+
+else
+    wget -q --no-check-certificate https://cmake.org/files/v3.16/cmake-3.16.4-Linux-x86_64.tar.gz \
+        && tar -xzf cmake-3.16.4-Linux-x86_64.tar.gz \
+        --exclude=bin/cmake-gui \
+        --exclude=doc/cmake \
+        --exclude=share/cmake-3.16/Help \
+        && cp -fR cmake-3.16.4-Linux-x86_64/* /usr \
+        && rm -rf cmake-3.16.4-Linux-x86_64 \
+        && rm cmake-3.16.4-Linux-x86_64.tar.gz
+fi
