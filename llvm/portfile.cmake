63677459149d4e3717e2646ee6bf14a0ceee301d
Lisa Ong <onglisa@microsoft.com>
2022-10-10 10:35:16 +0800

    15.0.2

--- llvm/portfile.cmake
+++ llvm/portfile.cmake
@@ -21,7 +21,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO llvm/llvm-project
     REF ${LLVM_VERSION}
-    SHA512 d518de4860bd953c4728f00cb52aba37e1e8c2a0825b0a6cc046494f2ad4d1e1d07d36524c2aca87c7f27a5c07c21cdcf18ac73a03c2eed813ca46d24f31f445
+    SHA512 69252b6e57c4803b4890f3bdd9c913d13d32c4e7f27b9b34d682f8c56eff4439a3ce934afea727f90957ed4ce5120b21f4ba8e7160605ab3123223624459a0e6
     HEAD_REF main
     PATCHES
     0001-Merged-PR-2213-mlir-Plumb-OpenMP-dialect-attributes-.patch
